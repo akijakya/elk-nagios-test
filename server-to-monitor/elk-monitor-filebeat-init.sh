e8407e5e0eab76a1aa7d53bb925132d905302b2f
akijakya <jakyandras@gmail.com>
2020-01-29 16:34:50 +0100

    started to work on server to be monitored with ELK Stack

--- server-to-monitor/elk-monitor-filebeat-init.sh
+++ server-to-monitor/elk-monitor-filebeat-init.sh
@@ -17,4 +17,17 @@ sudo apt install -yy docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
+# pulling and starting a docker container
 sudo systemctl start docker
+sudo docker pull akijakya/docker-single-test:latest
+sudo docker run -d -p 80:3000 akijakya/docker-single-test:latest
+
+# installing Filebeat
+# If you use Apt or Yum, you can install Filebeat from our repositories to update to the newest version more easily:
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt-get update && sudo apt-get install filebeat
+sudo systemctl enable filebeat
+# If your system does not use systemd then run:
+# sudo update-rc.d filebeat defaults 95 10
+
